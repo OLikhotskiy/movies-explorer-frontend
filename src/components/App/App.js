7750fb3a3bdea92b3e30424ce3002933463ad059
Oleg Likhotskiy <lihotsky777@gmail.com>
2023-06-13 21:23:03 +0200

    feat: App

---
+++ src/components/App/App.js
@@ -0,0 +1,32 @@
+import './App.css'
+import Main from '../Main/Main'
+import Movies from '../Movies/Movies'
+import Profile from '../Profile/Profile'
+import { Route, Routes } from 'react-router-dom'
+import SavedMovies from '../SavedMovies/SavedMovies'
+import Layout from '../Layout/Layout'
+import NotFound from '../NotFoundPage/NotFoundPage'//
+import Login from '../Login/Login'
+import Register from '../Register/Register'
+
+function App() {
+  return (
+    <div className="body">
+      <div className="page">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Main />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/saved-movies" element={<SavedMovies />} />
+          </Route>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/signin" element={<Login />} />
+          <Route path="/signup" element={<Register />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.js
@@ -0,0 +1,8 @@
+import './Header.css'
+import Logo from '../Logo/Logo'
+
+function Header() {
+
+}
+
+export default Header
